7d683ba44aecdf1a9edafb00da410127a54edddf
diligentlystereotypedinvestment <github.trespasses@alleeas.com>
2021-02-20 20:21:50 -0800

    modified:   polynomial.java

--- polynomial.java
+++ polynomial.java
@@ -1,14 +1,16 @@
+import java.util.ArrayList;
 import java.util.Random;
 
 public class polynomial {
 
 	private String problem;
 	private String answer;
+	private int[] coefficients = new int[3];
+	private int power;
 
-	public void Gen() {
-
+	public polynomial(){
 		Random rand = new Random();
-		int power = 2;
+		power = 2;
 		int choose = rand.nextInt(2);
 		if (choose == 0) {
 			power = 2;
@@ -16,6 +18,7 @@ public class polynomial {
 		problem = "";
 		for (int i = 2; i <= power; i++) {
 			int sign = rand.nextInt(2);
+			coefficients[i - 2] = sign;
 			if (sign == 0) {
 				problem = (rand.nextInt(6) + 1) + "x^" + i + " + " + problem;
 			}
@@ -30,10 +33,16 @@ public class polynomial {
 			problem = problem + (rand.nextInt(6) + 1) + "x" + " - ";
 		}
 		problem = "$" + problem + (rand.nextInt(9) + 1) + "$";
-		String tempProblem = problem;
-		choose = rand.nextInt(3);
+	}
+
+	public void Gen() {
+		int power = this.power;
+		Random rand = new Random();
+		polynomial problem = new polynomial();
+		String tempProblem = problem.toString();
+		int choose = rand.nextInt(3);
 		if (choose == 0) {
-			problem = "What is the sum of roots of " + problem + "?";
+			this.problem = "What is the sum of roots of " + problem + "?";
 			answer = simplify.getFraction(String.valueOf(-1 * Integer.valueOf(tempProblem.substring(8, 9))) + "/" + (tempProblem.substring(1, 2)));
 		}
 		if (choose == 1 && power == 2) {
@@ -43,7 +52,7 @@ public class polynomial {
 				int ac = (int) Math.pow(evenSquare, 2) / 4;
 				int a = PrimeDivisors.factor(ac).get(rand.nextInt(PrimeDivisors.factor(ac).size()));
 				int c = ac / a;
-				problem = "$" + a + "x^2 + " + evenSquare + "x + " + c + "$";
+				this.problem = "$" + a + "x^2 + " + evenSquare + "x + " + c + "$";
 				answer = "0";
 			}
 			if (decrimType == 1) {// 1
@@ -51,24 +60,46 @@ public class polynomial {
 				int ac = (int) (Math.pow(oddSquare, 2) - 1) / 4;
 				int a = PrimeDivisors.factor(ac).get(rand.nextInt(PrimeDivisors.factor(ac).size()));
 				int c = ac / a;
-				problem = "$" + a + "x^2 + " + oddSquare + "x + " + c + "$";
+				this.problem = "$" + a + "x^2 + " + oddSquare + "x + " + c + "$";
 				answer = "1";
 			}
-			problem = "What is the discriminate of " + problem + "?";
+			this.problem = "What is the discriminate of " + problem + "?";
 		} else if (choose == 1 && power == 3) {
-			problem = "What is the sum of the roots of " + problem + " taken two at a time?";
+			this.problem = "What is the sum of the roots of " + problem + " taken two at a time?";
 			answer = simplify.getFraction(tempProblem.substring(16, 17) + "/" + tempProblem.substring(1, 2));
 		}
 		if (choose == 2) {
-			problem = "What is the product of the roots of " + problem;
+			this.problem = "What is the product of the roots of " + problem;
 			answer = simplify.getFraction(tempProblem.substring(tempProblem.length() - 2, tempProblem.length() - 1) + "/" + ((int) Math.pow(-1, power)));
 		}
 
 	}
+
+	public void polynomialRoots(ArrayList<String> questions, ArrayList<String> answers, int i){
+		polynomial poly = new polynomial();
+		Random rand = new Random();
+		int choose = rand.nextInt(2);
+		int a = coefficients[0];
+		int b = coefficients[1];
+		int c = coefficients[2];
+		if(choose == 0){
+			questions.add("What is the lowest root of " + poly + "?");
+			answers.add(simplify.getFraction((-1 * b - (int) Math.sqrt(Math.pow(a, 2) - 4 * a * c)) + "/" + (2 * a)));
+		}
+		if(choose == 0){
+			questions.add("What is the largest root of " + poly + "?");
+			answers.add(simplify.getFraction((-1 * b + (int) Math.sqrt(Math.pow(a, 2) - 4 * a * c)) + "/" + (2 * a)));
+		}
+	}
+
 	public String getQuestion(){
 		return problem;
 	}
 	public String getAnswer(){
 		return answer;
+	}
+
+	public String toString(){
+		return problem;
 	}
 }
