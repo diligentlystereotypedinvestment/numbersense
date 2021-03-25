31e51bd4526a804be63181461fd5f4b1c99d54a7
JUICEMAN123 <JUICEMAN123@users.noreply.github.com>
2021-03-25 18:56:19 -0500

    try2

--- Number Sense/src/genRunner.java
+++ Number Sense/src/genRunner.java
@@ -4,7 +4,7 @@ public class genRunner
 {
 	public static void main (String[]args)
 	{
-		int teststuff = 1234214214;
+		int teststuff = 123;
 		ArrayList < String > questions = new ArrayList <> ();
 		ArrayList < String > answers = new ArrayList <> ();
 		System.out.
