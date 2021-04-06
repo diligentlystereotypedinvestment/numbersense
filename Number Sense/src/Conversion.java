647386e29c843a95c8359854414202547a6305cd
Vincent Tran <66981549+diligentlystereotypedinvestment@users.noreply.github.com>
2021-04-06 16:48:58 -0500

    Update Conversion.java

--- Number Sense/src/Conversion.java
+++ Number Sense/src/Conversion.java
@@ -69,7 +69,7 @@ public class Conversion {
 		int initial = (Integer) problem[0];
 		Unit iUnit = (Unit) problem[1];
 		Unit fUnit = (Unit) problem[2];
-		fUNit.init();
+		fUnit.init();
 		iUnit.init();
 		String question = "(" + problemNumber + ") What is " + initial + " " + iUnit.getName() + "s in " + fUnit.getName() + "s?";
 		String answers = String.valueOf(Unit.convert(initial, iUnit, fUnit));
